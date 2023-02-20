44bd7e765dbee4743d150a1c719d3db7b652df0f
no-name9 <rezanov.aleksandr10@gmail.com>
2023-02-20 20:50:43 +0400

    1

--- dist/js/main.js
+++
@@ -1,6 +0,0 @@
-for (var _i = 0, _a = document.querySelectorAll('.icon-heart'); _i < _a.length; _i++) {
-    var item = _a[_i];
-    item.addEventListener('click', function () {
-        this.classList.toggle('--active');
-    });
-}
